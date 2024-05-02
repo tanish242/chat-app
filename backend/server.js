13ca2c1a8bb314834ea028b0702d5ad1d9ac07ce
Tanish <tanishp242@gmail.com>
2024-05-02 18:18:33 +0530

    4th part done- socket.io

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,7 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
-
-const app = express();
+import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
@@ -17,19 +16,16 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 
-
 app.use("/api/auth",authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-
-
 
 // app.get("/",(req,res)=>{
 //     res.send("Hello World, how r u, i m fine");
 // }); 
 
 
-app.listen(PORT,()=> {
+server.listen(PORT,()=> {
     connectToMongoDB();
 console.log(`server is running on port ${PORT}`)
 });
--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -34,7 +34,7 @@ const MessageContainer = () => {
 export default MessageContainer;
 
 const NoChatSelected = () => {
-	const { authUser } = useAuthContext();
+  const { authUser } = useAuthContext();
 	return (
 		<div className='flex items-center justify-center w-full h-full'>
 			<div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
