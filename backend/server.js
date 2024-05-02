51b0d6d0d0a2ff6d8e75dc54a8488fcd4bdffa8d
Tanish <tanishp242@gmail.com>
2024-05-02 23:22:33 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -8,6 +9,8 @@ import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
+
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 
@@ -20,9 +23,12 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/",(req,res)=>{
-//     res.send("Hello World, how r u, i m fine");
-// }); 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 
 
 server.listen(PORT,()=> {
